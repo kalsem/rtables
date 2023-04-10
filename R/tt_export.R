26b11dc5bd35778a097a6df69b2261d18ef4733c
kalsem <109932262+kalsem@users.noreply.github.com>
2023-04-10 21:08:41 +0530

    Update tt_export.R
    Signed-off-by: kalsem <109932262+kalsem@users.noreply.github.com>

--- R/tt_export.R
+++ R/tt_export.R
@@ -559,7 +559,7 @@ export_as_rtf <- function(tt,
     tbls <- paginate_table(tt, font_family = font_family, font_size = font_size,
                            pg_width = true_width,
                            pg_height = true_height,
-                           margins = c(bottom = 2.5, left = 2.5, top = 2.5, right = 2.5),
+                           margins = c(bottom = 4, left = 4, top =4, right = 4),
                            lineheight = 1.25,
                            colwidths = colwidths,
                            ...)
@@ -570,7 +570,7 @@ export_as_rtf <- function(tt,
                                                                        pg_width = pg_width,
                                                                        pg_height = pg_height,
                                                                        font_size = font_size,
-                                                                       margins = c(top = 2.5, left = 2.5, bottom = 2.5, right = 2.5))))
+                                                                       margins = c(top = 4, left = 4, bottom = 4, right = 4))))
     restxt <- paste(rtftxts[[1]]$start,
                     paste(sapply(rtftxts, function(x) x$body), collapse = "\n{\\pard\\fs2\\par}\\page{\\pard\\fs2\\par}\n"),
                     rtftxts[[1]]$end)
